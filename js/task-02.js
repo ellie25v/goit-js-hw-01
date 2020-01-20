28bce7a389bcf3471279d0fe5faca1f2c84345dc
ellie25v <yelizarieva.ellie@gmail.com>
2020-01-20 16:11:56 +0200

    c

--- js/task-02.js
+++ js/task-02.js
@@ -3,17 +3,16 @@
 const total = 100;
 const ordered = prompt('Количество заказов');
 
-
 if (ordered === null) {
-    alert(`Cancelled`);
-    
-} else if (ordered >= total) {
+    alert(`Отменен заказ`);
+
+} else if (Number(ordered) >= total) {
     alert(`На складе недостаточно тoваров!`);
 
-} else if (ordered <= total) {
+} else if (Number(ordered) <= total) {
     alert(`Заказ оформлен, с вами свяжется менеджер`);
 
 } else {
-    alert(`What?`);
+    alert(`Число введено неправильно, заказ не принят`);
 
 }
